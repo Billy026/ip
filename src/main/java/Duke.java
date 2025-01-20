8b0e59af7bacac16763ba4fa4d397cf833efa53a
Billy026 <92001719+Billy026@users.noreply.github.com>
2025-01-20 20:30:03 +0800

    Changed logo to display name "Bob"

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,15 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        // Printing of logo
+        String logo = 
+                  " ____        _        \n"
+                + "|  _ \\      | |      \n"
+                + "| |_| |     | |       \n"
+                + "|    /  ___ | | __    \n"
+                + "|  _ \\ / _ \\| |/_ \\\n"
+                + "| |_| | |_| |  |_| |  \n"
+                + "|____/ \\___/|_|\\__/ \n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("___________________________________");
     }
 }
