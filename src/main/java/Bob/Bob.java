30f4ff812a0d6bb54df37ba65d0f5f54c3074836
Billy026 <92001719+Billy026@users.noreply.github.com>
2025-01-30 21:07:58 +0800

    Bob class: updated package name
    Class is not in main package Bob.
    
    Grouping all files in a main package Bob allows more efficient
    transfer of files.
    
    Let's put all exception files into Bob.

---
+++ src/main/java/Bob/Bob.java
@@ -0,0 +1,20 @@
+package Bob;
+
+import java.util.Scanner;
+
+import Bob.managers.UiManager;
+
+/**
+ * Main class.
+ */
+public class Bob {
+    private static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        UiManager uiManager = new UiManager();
+        uiManager.executeUi(sc);
+
+        // Clean up
+        sc.close();
+    }
+}
